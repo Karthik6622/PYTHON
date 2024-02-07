3ccb87c6729ccf0446b925f6bdba344495d0b115
Karthik6622 <karthikmca6622@gmail.com>
2024-02-08 02:27:18 +0530

    adding some files

--- streamlit/Streamlit_programs/PROJECTS2/chatbot2.py
+++ streamlit/Streamlit_programs/PROJECTS2/chatbot2.py
@@ -2,9 +2,11 @@ import streamlit as st
 st.title("Personal Chatbot")
 chat_history=[]
 input=st.chat_input(placeholder="Type somethings")
-
-if input:
+for i in chat_history:
+    st.write(i)
+if input!='exit':
     chat_history.append(input)
-    for i in chat_history:
-       st.write(i)
+elif input=='exit':
+    st.write("[GO TO PAGE](https://babylovechatbot.streamlit.app/)")
+    
     
