1b8c19313d6d2858e6bf1208fc056f3d74062d19
Karthik6622 <138970359+Karthik6622@users.noreply.github.com>
2024-02-08 09:14:50 +0000

    iii

--- streamlit/Streamlit_programs/PROJECTS2/chatbot2.py
+++ streamlit/Streamlit_programs/PROJECTS2/chatbot2.py
@@ -8,14 +8,18 @@ if input!='exit':
     chat_history.append(input)
 elif input=='exit':
     st.markdown("""
+                <html>
+                <head>
                 <script type="text/javascript">
                 function exitIframe() {
                     // Close or navigate back
                     window.parent.location.href = "https://babylovechatbot.streamlit.app/embed=true"
                 }
                 </script>
+                </head>
                 <body>
                 <button onclick="exitIframe()">Exit Iframe</button>
                 </body>
+                </html>
                 
                 """,unsafe_allow_html=True)
