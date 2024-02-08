bd4fb030f37aa14555a278c9bd3fbf995e35fbb4
Karthik6622 <karthikmca6622@gmail.com>
2024-02-08 17:19:00 +0530

    adding some files

--- streamlit/Streamlit_programs/PROJECTS2/chatbot2.py
+++ streamlit/Streamlit_programs/PROJECTS2/chatbot2.py
@@ -1,5 +1,22 @@
 import streamlit as st
-
+import json
+import pandas as pd
+#setting the streamlit page layout as wide
+st.set_page_config(layout="wide")
+@st.cache_data
+def algorithm():
+    with open("E:/PYTHON/streamlit/Streamlit_programs/PROJECTS2/personalintents.json") as f:
+         data=json.load(f)
+    tag=[]
+    input[]
+    response={}
+    for indents in data['intents2']:
+        response[indents['tag']]=indents['resonses']
+        for line in indents:
+            tag.append(line['tag'])
+            input.append(line['patterens'])
+    data2=pd.DataFrame({'input':input,'tag':tag})
+    st.dataframe(data2)
 st.title("Personal Chatbot")
 chat_history=[]
 input=st.chat_input(placeholder="Type somethings")
@@ -8,20 +25,6 @@ for i in chat_history:
 if input!='exit':
     chat_history.append(input)
 elif input=='exit':
-    st.markdown("""
-                <html>
-                <head>
-                <script type="text/javascript">
-                function exitIframe() {
-                    // Close or navigate back
-                    //window.parent.location.href = "https://babylovechatbot.streamlit.app/embed=true"
-                    alert("do you want go out?)
-                }
-                </script>
-                </head>
-                <body>
-                <button onclick="exitIframe()">Exit Iframe</button>
-                </body>
-                </html>
-                
-                """,unsafe_allow_html=True)
+    st.markdown('<a href="https://babylovechatbot.streamlit.app/?embed=true" target="_self">exit</a>', unsafe_allow_html=True)
+    
+    
